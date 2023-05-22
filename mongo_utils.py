4847cf43fa8fcfe5eb60ecf753110da589a801a6
pingrunhuang <pingrunhuang@gmail.com>
2023-05-22 11:05:20 +0800

    add mongo utils

---
+++ mongo_utils.py
@@ -0,0 +1,50 @@
+import yaml
+from pymongo import MongoClient, UpdateOne
+from typing import Optional
+import logging
+
+
+logger = logging.getLogger("dev")
+
+
+class MongoManger:    
+    setting_path = "configs/settings.yaml"
+
+    def __init__(self, db_name: Optional[str]) -> None:
+        with open(self.setting_path) as f:
+            config = yaml.safe_load(f)
+        if isinstance(config, dict):
+            url = config.get("mgo_url")
+            self.mgo_client = MongoClient(url)
+        else:
+            raise ValueError(
+                f"Please make sure format of {self.setting_path} is correct")
+        self.setup_db(db_name)
+    
+    def setup_db(self, db_name: Optional[str]):
+        if not getattr(self, "db", None):
+            self.db = self.mgo_client.get_database(db_name) 
+            logger.info(f"db setup to {self.db}")
+            
+
+    def batch_insert(self, data: list, clc: str):
+        if getattr(self, "db"):
+            self.db.get_collection(clc).insert_many(data)
+        else:
+            msg = "Please provide database name at initialization"
+            logger.error(msg)
+            raise ValueError(msg)
+
+    def batch_upsert(self, data: list, clc: str, key: str = "_id"):
+        to_update = [
+            UpdateOne({key: x[key]}, {"$set": x}, upsert=True) for x in data
+        ]
+        if self.db is not None:
+            result = self.db.get_collection(clc).bulk_write(to_update)
+            logger.info(f"Inserted count: {result.inserted_count}")
+            logger.info(f"Modified count: {result.modified_count}")
+            logger.info(f"Upserted count: {result.upserted_count}")
+        else:
+            msg = "Please call setup_db first"
+            logger.error(msg)
+            raise ValueError(msg)
